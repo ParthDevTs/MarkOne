387939c78f4b9f946c4c022d87d417a9e609e71e
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2022-07-12 13:50:03 +0530

    initialised npm

--- markOne.js
+++ markOne.js
@@ -10,46 +10,46 @@ var ans1 ="23";
 var ans2 ="sundara";
 var ans3 ="panda";
 
-print("_____Lets quiz you on How well you know parth!______");
+console.log("_____Lets quiz you on How well you know parth!______");
 var username = readlineSync.question("First, Tell me your name: ")
 
-print("Hello, ",username)
+console.log("Hello, ",username)
 
 
 
 var userAns= readlineSync.question(question1);
 if(userAns===ans1.toLowerCase()){
-    print("Correct Answer, You get 10 points!!");
+    console.log("Correct Answer, You get 10 points!!");
     score= score+10;
 }else{
-    print("Oh No!, That was an incorrect answer :(");
-    print("Dont worry, try again");
+    console.log("Oh No!, That was an incorrect answer :(");
+    console.log("Dont worry, try again");
 }
 
 var userAns= readlineSync.question(question2);
 if(userAns===ans2.toLowerCase()){
-    print("Correct Answer, You get 10 points!!");
+    console.log("Correct Answer, You get 10 points!!");
     score= score+10;
 }else{
-    print("Oh No!, That was an incorrect answer :(");
-    print("Dont worry, try again");
+    console.log("Oh No!, That was an incorrect answer :(");
+    console.log("Dont worry, try again");
 }
 
 var userAns= readlineSync.question(question3);
 if(userAns===ans3.toLowerCase()){
-    print("Correct Answer, You get 10 points!!");
+    console.log("Correct Answer, You get 10 points!!");
     score= score+10;
 }else{
-    print("Oh No!, That was an incorrect answer :(");
-    print("Dont worry, try again");
+    console.log("Oh No!, That was an incorrect answer :(");
+    console.log("Dont worry, try again");
 }
 
 
-print("Your total Score is: ", score);
+console.log("Your total Score is: ", score);
 if(score.valueOf()>10){
-    print("You know me really well!")
+    console.log("You know me really well!")
 }else{
-    print("Dont worry, you can try again! :( ")
+    console.log("Dont worry, you can try again! :( ")
 }
 
 
